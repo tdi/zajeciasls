3bd3afa462b6b095b529f2d8436c2b4f4505f54c
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2019-03-29 10:21:52 +0100

    zajecia

---
+++ handler.js
@@ -0,0 +1,33 @@
+const AWS = require("aws-sdk");
+
+const TABLE_NAME = process.env.DOGS_TABLE;
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+module.exports.hi = async (event, _ctx, cb) => {
+
+  if (event.queryStringParameters) {
+    const params = {
+      TableName: [TABLE_NAME],
+      Key: {
+        name: event.queryStringParameters.name
+      }
+    };
+    const dog = await docClient.get(params).promise();
+  
+    cb(null, sendRes(200, { dog }));
+  }
+
+  const dogs  = await docClient.scan({TableName: [TABLE_NAME]}).promise();
+  cb(null, sendRes(200, {dogs: dogs.Items}));
+ 
+};
+
+const sendRes = (status, body) => {
+  return {
+    statusCode: status,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
+  };
+};
